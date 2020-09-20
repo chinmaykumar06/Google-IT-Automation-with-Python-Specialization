e4395a02777d62350704c7df6ee9cd9bb8576cc5
ChinmayKumar <chinmaykumar195@gmail.com>
2020-09-20 21:38:33 +0530

    Add files via upload

---
+++ Course_2 Using Python to Interact with the Operating System/WEEK6/changeJane.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+f=open(sys.argv[1], "r")
+for line in f.readlines():
+    old_name = line.strip()
+    new_name = old_name.replace("jane","jdoe")
+    subprocess.run(["mv",old_name,new_name])
+f.close()
+
+
